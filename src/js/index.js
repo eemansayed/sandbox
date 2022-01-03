eb54c5406c262cfd46aa4dd69f18a80541e2d0d9
Eeman Sayed <eeman.sayed.12@gmail.com>
2022-01-03 17:44:26 +0000

    add event listener called toggleCross

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 const toggle = document.querySelector(".toggle");
 const toggleCross = document.querySelector(".toggle-cross");
+
+const toggleClickListener = (e) => {
+  toggle.style.display = "none";
+  toggleCross.style.display = "block";
+};
+toggle.addEventListener("click", toggleClickListener);
+
+const toggleCrossClickListener = (e) => {
+  toggle.style.display = "block";
+  toggleCross.style.display = "none";
+};
+
+toggleCross.addEventListener("click", toggleCrossClickListener);

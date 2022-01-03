5c773c8d4c32dc650fefd0b078ed6023a4eb1141
Eeman Sayed <eeman.sayed.12@gmail.com>
2022-01-03 17:51:33 +0000

    add const called fa-times

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,19 @@
 const toggle = document.querySelector(".toggle");
 const toggleCross = document.querySelector(".toggle-cross");
+const offCanvas = document.querySelector(".off-canvas");
+const faTimes = document.querySelector(".fa-times");
 
 const toggleClickListener = (e) => {
   toggle.style.display = "none";
   toggleCross.style.display = "block";
+  offCanvas.style.transform = "translateX(0)";
 };
 toggle.addEventListener("click", toggleClickListener);
 
 const toggleCrossClickListener = (e) => {
   toggle.style.display = "block";
   toggleCross.style.display = "none";
+  offCanvas.style.transform = "translateX(-100%)";
 };
 
 toggleCross.addEventListener("click", toggleCrossClickListener);

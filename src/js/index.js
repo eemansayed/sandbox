06ddb1b9e02a5e4cedd1195114582bfa141bf981
Eeman Sayed <eeman.sayed.12@gmail.com>
2022-01-04 11:28:45 +0000

    add const called toggleVisibility

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,14 @@ const offCanvas = document.querySelector(".off-canvas");
 const offCanvasCloseButton = document.querySelector(".off-canvas-close");
 
 const elements = [toggle, toggleCross, offCanvas];
+
+const toggleVisibility = () => {
+  elements.forEach((element) => {
+    element.classList.toggle("visible");
+    element.classList.toggle("invisible");
+  });
+};
+
+elements.forEach((element) =>
+  element.addEventListener("click", toggleVisibility)
+);

241730fe7bdd361cb23b3ff2dc3bcc715ed3ef88
Eeman Sayed <eeman.sayed.12@gmail.com>
2022-01-04 11:14:58 +0000

    add const called elements

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,5 @@ const toggle = document.querySelector(".toggle");
 const toggleCross = document.querySelector(".toggle-cross");
 const offCanvas = document.querySelector(".off-canvas");
 const offCanvasCloseButton = document.querySelector(".off-canvas-close");
+
+const elements = [toggle, toggleCross, offCanvas];

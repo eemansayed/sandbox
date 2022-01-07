5d653b332c06fac1595ca645e6431f2e0e2a6b43
Eeman Sayed <eeman.sayed.12@gmail.com>
2022-01-07 17:33:53 +0000

    add function

--- src/js/index.js
+++ src/js/index.js
@@ -15,3 +15,43 @@ const toggleVisibility = () => {
 elements.forEach((element) =>
   element.addEventListener("click", toggleVisibility)
 );
+
+const visibilityChangeHandler = () => {
+  const element = document.querySelector(".semi-circles");
+  const progress = document.querySelectorAll(
+    ".pros-section .container .semi-circles svg"
+  );
+
+  const isElementInView = () => {
+    const rectangle = element.getBoundingClientRect();
+
+    return (
+      rectangle.top >= 0 &&
+      rectangle.left >= 0 &&
+      rectangle.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rectangle.right <=
+        (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  let wasVisible;
+  let isVisible = isElementInView(element);
+  if (isVisible != wasVisible) {
+    wasVisible = isVisible;
+    progress.forEach((el) => {
+      el.querySelectorAll("path")[1].classList.remove("progress");
+      setTimeout(
+        () => el.querySelectorAll("path")[1].classList.add("progress"),
+        1
+      );
+    });
+  }
+};
+
+if (window.addEventListener) {
+  addEventListener("DOMContentLoaded", visibilityChangeHandler, false);
+  addEventListener("load", visibilityChangeHandler, false);
+  addEventListener("scroll", visibilityChangeHandler, false);
+  addEventListener("resize", visibilityChangeHandler, false);
+}
